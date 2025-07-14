8ecbb7308dd5f5ecd07c426423d804d4f532bbbf
Matia Hasmasan <83250771+matiahasmasan@users.noreply.github.com>
2025-07-14 11:44:04 +0300

    Fake Dynamic variables

--- app/livewire/Users.php
+++ app/livewire/Users.php
@@ -6,10 +6,27 @@ use Livewire\Component;
 
 class Users extends Component
 {
-    public int $n = 1000;
+    public array $users = [
+        [
+            'id' => 1,
+            'email' => 'john.doe@email.com',
+            'companie' => 'SRL',
+            'rol' => 'Administrator',
+            'subordonat' => '-',
+        ],
+        [
+            'id' => 2,
+            'email' => 'jane.smith@email.com',
+            'companie' => 'SRL',
+            'rol' => 'Utilizator',
+            'subordonat' => 'john.doe@email.com',
+        ],
+    ];
 
     public function render()
     {
-        return view('livewire.users');
+        return view('livewire.users', [
+            'users' => $this->users,
+        ]);
     }
 }
