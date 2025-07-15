6c105ef7f09e3ae85863c55f237004a4aaaf4ed3
Matia Hasmasan <83250771+matiahasmasan@users.noreply.github.com>
2025-07-15 12:17:36 +0300

    Added modal controller

---
+++ app/livewire/modal/AddRoleModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Modal;
+
+use Livewire\Component;
+
+class AddRoleModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.add-role-modal');
+    }
+}
