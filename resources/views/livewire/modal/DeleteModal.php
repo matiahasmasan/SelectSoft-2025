4b6f5742ea4128b3803ab6cd22d37c6a63caf250
Matia Hasmasan <83250771+matiahasmasan@users.noreply.github.com>
2025-07-15 12:13:41 +0300

    Add files via upload

---
+++ resources/views/livewire/modal/DeleteModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Modal;
+
+use Livewire\Component;
+
+class DeleteModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.delete-modal');
+    }
+}
---
+++ resources/views/livewire/modal/EditModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Modal;
+
+use Livewire\Component;
+
+class EditModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.edit-modal');
+    }
+}
