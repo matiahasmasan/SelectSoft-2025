f64fd282798950d1e52eec24c7cd8d858af41873
Matia Hasmasan <83250771+matiahasmasan@users.noreply.github.com>
2025-07-17 14:03:54 +0300

    Modals Dynamic Dropdown

--- app/livewire/Permissions.php
+++ app/livewire/Permissions.php
@@ -1,52 +1,64 @@
 <?php
 
 namespace App\Livewire;
-
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Permissions extends Component
 {
-    public array $permissions = [
-        [
-            'category' => 'Gestionare utilizatori',
-            'items' => [
-                [
-                    'name' => 'Vizualizare utilizatori',
-                    'rights' => ['R', 'W', 'U', 'D'],
-                ],
-                [
-                    'name' => 'Adaugă utilizator',
-                    'rights' => ['R', 'W', 'U', 'D'],
-                ],
-                [
-                    'name' => 'Șterge utilizator',
-                    'rights' => ['R', 'W', 'U', 'D'],
-                ],
-            ],
-        ],
-        [
-            'category' => 'Gestionare rapoarte',
-            'items' => [
-                [
-                    'name' => 'Vizualizare rapoarte',
-                    'rights' => ['R', 'W', 'U', 'D'],
-                ],
-                [
-                    'name' => 'Generează raport',
-                    'rights' => ['R', 'W', 'U', 'D'],
-                ],
-                [
-                    'name' => 'Șterge raport',
-                    'rights' => ['R', 'W', 'U', 'D'],
+    public \Illuminate\Support\Collection $permissions;
+    //public \Illuminate\Support\Collection $allRoles; //dropdown modals
+
+    public function mount()
+    {
+        $this->permissions = collect([
+            [
+                'category' => 'Gestionare utilizatori',
+                'items' => [
+                    [
+                        'name' => 'Vizualizare utilizatori',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
+                    [
+                        'name' => 'Adaugă utilizator',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
+                    [
+                        'name' => 'Șterge utilizator',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
                 ],
             ],
-        ],
-    ];
+            [
+                'category' => 'Gestionare rapoarte',
+                'items' => [
+                    [
+                        'name' => 'Vizualizare rapoarte',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
+                    [
+                        'name' => 'Generează raport',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
+                    [
+                        'name' => 'Șterge raport',
+                        'rights' => ['R', 'W', 'U', 'D'],
+                    ],
+                ],
+            ],
+        ]);
+        /* 
+        $rolesComponent = new Roles();
+        $rolesComponent->mount();
+        $this->allRoles = $rolesComponent->allRoles;*/
+        
+    }
 
     public function render()
     {
         return view('livewire.permissions', [
             'permissions' => $this->permissions,
+            //'roles' => $this->allRoles,
         ]);
     }
 }
--- app/livewire/Users.php
+++ app/livewire/Users.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use App\Livewire\Roles;
 
 class Users extends Component
 {
@@ -12,184 +14,112 @@ class Users extends Component
 
     public int $perPage = 5; // Users per page
 
-    public array $allUsers = [
-        [
-            'id' => 1,
-            'email' => 'john.doe@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Administrator',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 2,
-            'email' => 'jane.smith@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 3,
-            'email' => 'bob.wilson@email.com',
-            'companie' => 'SA',
-            'rol' => 'Manager',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 4,
-            'email' => 'alice.brown@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'bob.wilson@email.com',
-        ],
-        [
-            'id' => 5,
-            'email' => 'charlie.davis@email.com',
-            'companie' => 'SA',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 6,
-            'email' => 'diana.evans@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Manager',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 7,
-            'email' => 'frank.garcia@email.com',
-            'companie' => 'SA',
-            'rol' => 'Utilizator',
-            'subordonat' => 'diana.evans@email.com',
-        ],
-        [
-            'id' => 8,
-            'email' => 'grace.harris@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 9,
-            'email' => 'henry.jones@email.com',
-            'companie' => 'SA',
-            'rol' => 'Administrator',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 10,
-            'email' => 'iris.king@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'henry.jones@email.com',
-        ],
-        [
-            'id' => 11,
-            'email' => 'jack.lee@email.com',
-            'companie' => 'SA',
-            'rol' => 'Manager',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 12,
-            'email' => 'karen.miller@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'jack.lee@email.com',
-        ],
-        [
-            'id' => 1,
-            'email' => 'john.doe@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Administrator',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 2,
-            'email' => 'jane.smith@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 3,
-            'email' => 'bob.wilson@email.com',
-            'companie' => 'SA',
-            'rol' => 'Manager',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 4,
-            'email' => 'alice.brown@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'bob.wilson@email.com',
-        ],
-        [
-            'id' => 5,
-            'email' => 'charlie.davis@email.com',
-            'companie' => 'SA',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 6,
-            'email' => 'diana.evans@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Manager',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 7,
-            'email' => 'frank.garcia@email.com',
-            'companie' => 'SA',
-            'rol' => 'Utilizator',
-            'subordonat' => 'diana.evans@email.com',
-        ],
-        [
-            'id' => 8,
-            'email' => 'grace.harris@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 9,
-            'email' => 'henry.jones@email.com',
-            'companie' => 'SA',
-            'rol' => 'Administrator',
-            'subordonat' => '-',
-        ],
-        [
-            'id' => 10,
-            'email' => 'iris.king@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'henry.jones@email.com',
-        ],
-        [
-            'id' => 11,
-            'email' => 'jack.lee@email.com',
-            'companie' => 'SA',
-            'rol' => 'Manager',
-            'subordonat' => 'john.doe@email.com',
-        ],
-        [
-            'id' => 12,
-            'email' => 'karen.miller@email.com',
-            'companie' => 'SRL',
-            'rol' => 'Utilizator',
-            'subordonat' => 'jack.lee@email.com',
-        ],
-    ];
+    public Collection $allUsers;
+    public \Illuminate\Support\Collection $allRoles; //dropdown modals
+
+    public function mount()
+    {
+        $this->allUsers = collect([
+            [
+                'id' => 1,
+                'email' => 'john.doe@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Administrator',
+                'subordonat' => '-',
+            ],
+            [
+                'id' => 2,
+                'email' => 'jane.smith@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Utilizator',
+                'subordonat' => 'john.doe@email.com',
+            ],
+            [
+                'id' => 3,
+                'email' => 'bob.wilson@email.com',
+                'companie' => 'SA',
+                'rol' => 'Manager',
+                'subordonat' => 'john.doe@email.com',
+            ],
+            [
+                'id' => 4,
+                'email' => 'alice.brown@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Utilizator',
+                'subordonat' => 'bob.wilson@email.com',
+            ],
+            [
+                'id' => 5,
+                'email' => 'charlie.davis@email.com',
+                'companie' => 'SA',
+                'rol' => 'Utilizator',
+                'subordonat' => 'john.doe@email.com',
+            ],
+            [
+                'id' => 6,
+                'email' => 'diana.evans@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Manager',
+                'subordonat' => '-',
+            ],
+            [
+                'id' => 7,
+                'email' => 'frank.garcia@email.com',
+                'companie' => 'SA',
+                'rol' => 'Utilizator',
+                'subordonat' => 'diana.evans@email.com',
+            ],
+            [
+                'id' => 8,
+                'email' => 'grace.harris@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Utilizator',
+                'subordonat' => 'john.doe@email.com',
+            ],
+            [
+                'id' => 9,
+                'email' => 'henry.jones@email.com',
+                'companie' => 'SA',
+                'rol' => 'Administrator',
+                'subordonat' => '-',
+            ],
+            [
+                'id' => 10,
+                'email' => 'iris.king@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Utilizator',
+                'subordonat' => 'henry.jones@email.com',
+            ],
+            [
+                'id' => 11,
+                'email' => 'jack.lee@email.com',
+                'companie' => 'SA',
+                'rol' => 'Manager',
+                'subordonat' => 'john.doe@email.com',
+            ],
+            [
+                'id' => 12,
+                'email' => 'karen.miller@email.com',
+                'companie' => 'SRL',
+                'rol' => 'Utilizator',
+                'subordonat' => 'jack.lee@email.com',
+            ],
+        ]);
+        // Load Roles.php
+        $rolesComponent = new Roles();
+        $rolesComponent->mount();
+        $this->allRoles = $rolesComponent->allRoles;
+    }
+
+    
 
     public function render()
     {
         // pagination
         $currentPage = $this->getPage();
-        $total = count($this->allUsers);
+        $total = $this->allUsers->count();
         $offset = ($currentPage - 1) * $this->perPage;
-        $items = array_slice($this->allUsers, $offset, $this->perPage);
+        $items = $this->allUsers->slice($offset, $this->perPage)->values()->all();
         $users = new LengthAwarePaginator(
             $items,
             $total,
@@ -204,6 +134,7 @@ class Users extends Component
         // render
         return view('livewire.users', [
             'users' => $users,
+            'roles' => $this->allRoles,
         ]);
     }
 }
